e9e636afe33f1e7c042aa1804cf397d92bd7430c
Russ Miles <russ@russmiles.com>
2013-07-17 12:18:01 +0100

    Added initial implementation of specification - not working yet

--- start/src/main/clojure/test.clj
+++ start/src/main/clojure/test.clj
@@ -1,13 +1,16 @@
 (ns test
    (:require [clj-drone.core :refer :all]
              [clj-drone.navdata :refer :all])
-	(:gen-class))
+	(:gen-class
+    :name com.simplicityitself.DroneFacade))
+
+(defn -testDrone [this]
+  (drone-initialize)
+
+  (drone-init-navdata)
+
+  (end-navstream))
 
 (defn -main [& args]
-	 (drone-initialize)
-
-	 (drone-init-navdata)
-	
-	 (end-navstream)
 	
 	 (println "Hello World"))
