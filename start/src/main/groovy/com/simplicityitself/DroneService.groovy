797532060c6df20dcbb58fea2d8ec60003b59ab3
David Dawson <david.dawson@dawsonsystems.com>
2013-07-18 15:20:32 +0100

    Add logging

--- start/src/main/groovy/com/simplicityitself/DroneService.groovy
+++ start/src/main/groovy/com/simplicityitself/DroneService.groovy
@@ -1,5 +1,8 @@
 package com.simplicityitself
 
+import groovy.util.logging.Slf4j
+
+@Slf4j
 class DroneService {
 
    Drone drone
@@ -7,20 +10,22 @@ class DroneService {
     // TODO Expose these methods to JMX for 'integration' purposes
     // TODO Attempt to send an email if status indicates an emergency has occurred
     // TODO Code to unpack the status' will be rife in here.
-    // TODO Add regular logging
     // TODO Add logging of commands such that they can be replayed 'in the future'
 
    def takeOffAndHoverForSeconds(int hoverSeconds, int takeOffSeconds) {
+     log.info("Taking off for [$takeOffSeconds] and hovering for [$hoverSeconds]")
        drone.takeOff(takeOffSeconds)
        drone.hover(hoverSeconds)
        drone.getCurrentStatus()
     }
 
     def climbForSecondsAtSpecifiedRate(float rate, int seconds) {
+      log.info "Climbing for $seconds at $rate"
         drone.climb(seconds, rate)
     }
 
     def closeSession() {
+      log.warn "Drone is instructed to land"
         drone.land()
     }
 
