b9e5f9b3a96b090b674c4b331235f80a1476207a
Marcin Floryan <marcin.floryan@gmail.com>
2013-07-19 12:20:27 +0100

    renamed EventLog to FlightLog

--- start/src/main/groovy/com/simplicityitself/events/FlightLog.groovy
+++ start/src/main/groovy/com/simplicityitself/events/FlightLog.groovy
@@ -3,7 +3,7 @@ package com.simplicityitself.events
 import groovy.util.logging.Slf4j
 
 @Slf4j
-class EventLog {
+class FlightLog {
 
   public void logTakeOff(int hoverSeconds, int takeOffSeconds) {
     log.debug "REPLAY: takeOffAndHoverForSeconds($hoverSeconds, $takeOffSeconds)"
